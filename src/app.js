d833bb090e51b8f01370be6787e68a6a1144e31a
Juaki7 <163038670+Juaki7@users.noreply.github.com>
2024-03-25 21:15:51 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hola");
+};
+let generateExcuse = () => {
+  let pronombre = ["A", "The"];
+  let sujeto = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+  let verbo = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let posicion = ["homework", "toe", "car", "shoe"];
+  let donde = ["on the street", "in my house", "in my driveway"];
+
+  let pronIndx = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndx = Math.floor(Math.random() * sujeto.length);
+  let verboIndex = Math.floor(Math.random() * verbo.length);
+  let posicionIndex = Math.floor(Math.random() * posicion.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    pronombre[pronIndx] +
+    " " +
+    sujeto[sujetoIndx] +
+    " " +
+    verbo[verboIndex] +
+    " " +
+    posicion[posicionIndex] +
+    " " +
+    donde[dondeIndex]
+  );
 };
